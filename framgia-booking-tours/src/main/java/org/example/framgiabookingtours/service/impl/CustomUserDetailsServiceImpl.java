e92b231fa0290d5f94229a3abe57372adc509bfa
Hồ Thanh Huy <138790604+ywhnaht@users.noreply.github.com>
2025-11-25 21:20:58 +0700

    Refs [TASK][95492] Implement Core Logic (JWT Utils, UserDetailsService, AuthEntryPoint) (#19)
    * Implement Core Logic (JWT Utils, UserDetailsService, AuthEntryPoint)
    
    * fix logic service and impl
    
    * fix auth entry point

---
+++ framgia-booking-tours/src/main/java/org/example/framgiabookingtours/service/impl/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package org.example.framgiabookingtours.service.impl;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.example.framgiabookingtours.dto.CustomUserDetails;
+import org.example.framgiabookingtours.entity.User;
+import org.example.framgiabookingtours.repository.UserRepository;
+import org.example.framgiabookingtours.service.CustomUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
+    UserRepository userRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Email not found."));
+        return new CustomUserDetails(user);
+    }
+}
